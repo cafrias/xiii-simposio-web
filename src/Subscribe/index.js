6657099522322d3b337d3ed6a50a3cef5553eb87
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-12-08 13:57:02 -0300

    Add basic input fields with React.Fragment

--- src/Subscribe/index.js
+++ src/Subscribe/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Form, Text } from 'react-form'
 
+const TextWithLabel = ({label, id}) =>
+  <React.Fragment>
+    <label htmlFor={id}>{label}</label>
+    <Text field={id} id={id} />
+  </React.Fragment>
+
 const SubscribeForm = props =>
   <form onSubmit={props.formApi.submitForm} id="form1">
-    <label htmlFor="hello">Hello World</label>
-    <Text field="hello" id="hello" />
+    <fieldset>
+      <TextWithLabel id="apellido" label="Apellido(s)" />
+      <TextWithLabel id="nombre" label="Nombre(s)" />
+    </fieldset>
     <button type="submit" className="btn btn-primary">Submit</button>
   </form>
 
