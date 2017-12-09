88ad462cd74e8f4480902759c155ffebcec85834
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-12-09 17:51:33 -0300

    Add root reducer

---
+++ src/ducks.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+
+// ACTIONS _____________________________________________________________________
+
+// ACTIONS CREATORS ____________________________________________________________
+
+// REDUCERS ____________________________________________________________________
+
+// ROOT REDUCER ________________________________________________________________
+export default combineReducers({
+  form: formReducer
+})
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import rootReducer from './ducks'
+
+const store = createStore(rootReducer)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
+registerServiceWorker()
