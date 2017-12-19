0cf273cc2dd421e0ff9f6292de67fd6bb9ee08ee
friasdesign <cfrias@localhost.localdomain>
2017-12-19 10:13:33 -0300

    Add flow types to form

---
+++ src/Subscribe/errors.js
@@ -0,0 +1,18 @@
+// @flow
+/*
+This file contains all error types and messages they have.
+*/
+
+type Required = {
+  type: 'required',
+  message: string
+}
+
+type Invalid = {
+  type: 'invalid',
+  message: string
+}
+
+export type ValidationError =
+  | Required
+  | Invalid
