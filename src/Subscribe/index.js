2370d3797873f4170520996c0a07bcdd93c60d1f
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-12-19 07:38:25 -0300

    Add icons to form

--- src/Subscribe/index.js
+++ src/Subscribe/index.js
@@ -1,13 +1,46 @@
+// @flow
 import React from 'react'
-import { Field, reduxForm } from 'redux-form'
 
-const Input = ({ id, label, type = 'text', className = '' }) =>
-  <div className={`field ${className}`}>
-    <label htmlFor={id} className="label">{label}</label>
-    <div className="control has-icons-right">
-      <Field id={id} name={id} component="input" type={type} className="input"/>
+const Explanation = ({ message }) =>
+  <small>{ message }</small>
+
+const LeftIcon = ({ className = '' }) =>
+  <div class="icon is-small is-left">
+    <i class={`fa ${className}`}></i>
+  </div>
+
+const Input = ({ id, label, type = 'text', className = '', icon }) => {
+  const controlClassName = `control has-icons-right ${ icon ? 'has-icons-left' : '' }`
+  return (
+    <div className={`field ${className}`}>
+      <label htmlFor={id} className="label">{label}</label>
+      <div className={controlClassName}>
+        <input id={id} name={id} component="input" type={type} className="input"/>
+        { type === 'number' ? Explanation({ message: '* Sólo números' }) : null }
+        { icon ? <LeftIcon className={icon} /> : null }
+      </div>
+    </div>
+  )
+}
+
+const Select = ({ id, label, type = 'text', className = '' }) =>
+<div className={`field ${className}`}>
+  <label htmlFor={id} className="label">{label}</label>
+  <div className="control has-icons-left has-icons-right" style={{ width: '100%' }}>
+    <div className="select">
+      <select>
+        <option selected>Argentina</option>
+        <option>Bolivia</option>
+        <option>Brasil</option>
+        <option>Chile</option>
+        <option>Paraguay</option>
+        <option>Uruguay</option>
+        <option>Otro</option>
+      </select>
+      <LeftIcon className="fa-globe"/>
     </div>
   </div>
+</div>
 
 const SubmitButton = () =>
   <div className="control">
@@ -23,34 +56,29 @@ const ContactForm = ({handleSubmit}) =>
         <Input id="nombre" label="Nombre(s)" className="column" />
       </div>
       <div className="columns">
-        <Input id="documento" label="Documento de Identidad" className="column" />
+        <Input id="documento" type="number" label="Documento de Identidad" className="column" />
         <div className="column columns">
-          <Input id="telefono" label="Teléfono" className="column" />
-          <Input id="celular" label="Celular" className="column" />
+          <Input id="telefono" type="number" label="Teléfono" className="column" icon="fa-phone" />
+          <Input id="celular" type="number" label="Celular" className="column" icon="fa-mobile" />
         </div>
       </div>
       <div className="columns">
-        <Input id="fax" label="Fax" className="column" />
-        <Input id="email" label="E-mail" className="column" />
+        <Input id="fax" label="Fax" className="column" icon="fa-fax" />
+        <Input id="email" label="E-mail" className="column" icon="fa-envelope" />
       </div>
       <div className="columns">
-        <Input id="direccion" label="Dirección" className="column" />
+        <Input id="direccion" label="Dirección" className="column" icon="fa-map-marker" />
         <Input id="zip" label="Codigo Postal" type="number" className="column" />
-        <Input id="localidad" label="Localidad" className="column" />
-        <Input id="pais" label="País" className="column" />
+        <Input id="localidad" label="Localidad" className="column" icon="fa-building" />
+        <Select id="pais" label="País" className="column" />
       </div>
     </fieldset>
     <SubmitButton />
   </form>
 
-const ContactFormRedux = reduxForm({
-  form: 'contact',
-  onSubmit: (values) => alert(values.firstName)
-})(ContactForm)
-
 const Subscribe = () =>
   <section className="section">
-    <ContactFormRedux/>
+    <ContactForm/>
   </section>
 
 export default Subscribe
--- src/index.js
+++ src/index.js
@@ -3,12 +3,6 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
 
-import rootReducer from './ducks'
-
-const store = createStore(rootReducer)
-
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
