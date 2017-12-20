d448fcf8ae9e874d35f0191b11154e6ab199a787
Carlos Frias <carlosfrias@Carloss-MacBook-Air.local>
2017-12-20 07:54:37 -0300

    Fix layout

--- src/Subscribe/index.js
+++ src/Subscribe/index.js
@@ -16,25 +16,59 @@ const ContactForm = ({handleSubmit}) =>
     <fieldset className="box">
       <legend className="label has-text-centered">Información de Contacto</legend>
       <div className="columns">
-        <Field id="apellido" label="Apellido(s)" className="column" control={Input} />
-        <Field id="nombre" label="Nombre(s)" className="column" control={Input} />
-      </div>
-      <div className="columns">
-        <Field id="documento" type="number" label="Documento de Identidad" className="column" control={Input} />
-        <div className="column columns">
-          <Field id="telefono" type="number" label="Teléfono" className="column" icon="fa-phone" control={Input} />
-          <Field id="celular" type="number" label="Celular" className="column" icon="fa-mobile" control={Input} />
+        <div className="column">
+          <Field id="apellido" label="Apellido(s)" control={Input} />
+        </div>
+        <div className="column">
+          <Field id="nombre" label="Nombre(s)" control={Input} />
         </div>
       </div>
       <div className="columns">
-        <Field id="fax" label="Fax" className="column" icon="fa-fax" control={Input} />
-        <Field id="email" label="E-mail" className="column" icon="fa-envelope" control={Input} />
+        <div className="column">
+          <Field id="documento" type="number" label="Documento de Identidad" control={Input} />
+        </div>
+        <div className="column">
+          <div className="columns">
+            <div className="column">
+              <Field id="telefono" type="number" label="Teléfono" icon="fa-phone" control={Input} />
+            </div>
+            <div className="column">
+              <Field id="celular" type="number" label="Celular" icon="fa-mobile" control={Input} />
+            </div>
+          </div>
+        </div>
       </div>
       <div className="columns">
-        <Field id="direccion" label="Dirección" className="column" icon="fa-map-marker" control={Input} />
-        <Field id="zip" label="Codigo Postal" type="number" className="column" control={Input} />
-        <Field id="localidad" label="Localidad" className="column" icon="fa-building" control={Input} />
-        <Field id="pais" label="País" className="column" control={Select} />
+        <div className="column">
+          <Field id="fax" label="Fax" icon="fa-fax" control={Input} />
+        </div>
+        <div className="column">
+          <Field id="email" label="E-mail" icon="fa-envelope" control={Input} />
+        </div>
+      </div>
+      <div className="columns">
+        <div className="column">
+          <div className="columns">
+            <div className="column">
+              <Field id="direccion" label="Dirección" icon="fa-map-marker" control={Input} />
+            </div>
+            <div className="column">
+              <Field id="zip" label="Codigo Postal" type="number" control={Input} />
+            </div>
+          </div>
+        </div>
+        <div className="column">
+          <div className="columns">
+            <div className="column">
+              <Field id="localidad" label="Localidad" icon="fa-building" control={Input} />
+            </div>
+            <div className="column">
+              <Field id="pais" label="País" control={Select} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="column">
       </div>
     </fieldset>
     <SubmitButton />
