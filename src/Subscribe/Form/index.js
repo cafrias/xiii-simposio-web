3224821950649276fb45ea4bc8b34aac3a60f0b2
friasdesign <cfrias@localhost.localdomain>
2017-12-26 12:28:22 -0300

    Add submit

--- src/Subscribe/Form/index.js
+++ src/Subscribe/Form/index.js
@@ -67,33 +67,50 @@ class SubscriptionForm extends React.Component<SubscriptionFormProps, Subscripti
     return newState
   }
 
+  generatePostRequest = () => {
+    const stateFields = this.state.fields
+    const requestObject = {}
+
+    for (const key in stateFields) {
+      if (stateFields.hasOwnProperty(key)) {
+        const element = stateFields[key]
+        requestObject[key] = element.value
+      }
+    }
+
+    return requestObject
+  }
+
   submitHandler = (e: SyntheticEvent<HTMLButtonElement>): void => {
     e.preventDefault()
     const newState = Object.assign({}, this.state, {
       loading: true
     })
-    this.setState(newState)
+    this.setState(newState, () => {
+      const request = this.generatePostRequest()
 
-    fetch(endpointURI, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(this.state.fields)
-    }).then(response => {
-      console.log(response.status)
-      const newState = Object.assign({}, this.state, {
-        loading: false
+      fetch(endpointURI, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(request)
+      }).then(response => {
+        console.log(response.status)
+        const newState = Object.assign({}, this.state, {
+          loading: false
+        })
+        this.setState(newState)
+        if(response.status >= 400 && response.status <= 500)
+          return response.json()
+      }).then(json => {
+        console.log('Response body:', json)
+      }).catch(err => {
+        console.error(err)
       })
-      this.setState(newState)
-      return response.json()
-    }).then(json => {
-      console.log('Response body:', json)
-    }).catch(err => {
-      console.error(err)
     })
 
-    console.log('Submitted')
+    console.log('Submitting ...')
   }
 
   changeHandler = (field: FormField, event: SyntheticEvent<TargetElements>) => {
