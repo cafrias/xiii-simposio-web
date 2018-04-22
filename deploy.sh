65b26f7da1411408d479496e7107679c65a44969
friasdesign <carlos.a.frias@gmail.com>
2018-04-22 16:04:52 -0300

    Refactor to add production command

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,13 @@
 
 yarn build
 
-aws s3 cp build/ s3://simposio-web.cfrias.com.ar/ --recursive
+if [ "$1" == "production" ]; then
+  echo 'Pushing to production ...'
+  aws s3 cp build/ s3://simposioenushuaia.com.ar/ --recursive
+  echo 'Pushing to production ... DONE!'
+else
+  echo 'Pushing to development ...'
+  aws s3 cp build/ s3://simposio-web.cfrias.com.ar/ --recursive
+  echo 'Pushing to development ... DONE!'
+fi
+
