34fe35239c75addd57c2424075c9b922837d42d5
friasdesign <carlos.a.frias@gmail.com>
2018-05-05 18:12:31 -0300

    Add env

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ yarn build
 
 if [ "$1" == "production" ]; then
   echo 'Pushing to production ...'
-  aws s3 cp build/ s3://simposioenushuaia.com.ar/ --recursive
+  aws s3 cp build/ s3://simposioenushuaia.com.ar/ --recursive --profile untdf
   echo 'Pushing to production ... DONE!'
 else
   echo 'Pushing to development ...'
