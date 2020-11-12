71d5684f5c79425a3abfb4fff83aa36ece87b165
Carlos Frias <cfrias@sparkdigital.com>
2020-11-12 10:26:11 -0300

    Update assets

--- src/pages/Home/Sponsor/_data.ts
+++ src/pages/Home/Sponsor/_data.ts
@@ -1,43 +1,24 @@
 // IMAGES ______________________________________________________________________
-import logo01 from './assets/logo-01.svg'
-import logo02 from './assets/logo-02.png'
-import logo03 from './assets/logo-03.jpg'
-import logo04 from './assets/logo-04.gif'
-import logo05 from './assets/logo-05.jpg'
-import logo06 from './assets/logo-06.png'
+import logoCpctf from './assets/logo-cpctf.gif'
+import logoCodece from './assets/logo-codece.png'
+import logoJuc from './assets/logo-juc.png'
 
 export const sponsors = [
   {
-    id: '01',
-    logo: logo01,
-    alt: 'Logo Legislatura de la Provincia de Tierra del Fuego'
+    id: 'cpctf',
+    logo: logoCpctf,
+    alt: 'Logo del "Consejo Profesional de Ciencias Económicas de Tierra del Fuego"'
   },
   {
-    id: '02',
-    logo: logo02,
-    alt: 'Logo Gobierno de la Provincia de Tierra del Fuego'
+    id: 'codece',
+    logo: logoCodece,
+    alt: 'Logo de "CODECE"'
   },
   {
-    id: '03',
-    logo: logo03,
-    alt: 'Logo Banco de la Provincia de Tierra del Fuego'
-  },
-  {
-    id: '04',
-    logo: logo04,
-    alt: 'Logo Consejo Profesional de Ciencias Económicas de Tierra del Fuego'
+    id: 'juc',
+    logo: logoJuc,
+    alt: 'Logo de "Juc"'
   },
 ]
 
-export const collaborators = [
-  {
-    id: '05',
-    logo: logo05,
-    alt: 'Logo Laguna Negra Chocolatería'
-  },
-  {
-    id: '06',
-    logo: logo06,
-    alt: 'Logo Rumbo Sur SRL'
-  },
-]
+export const collaborators = []
