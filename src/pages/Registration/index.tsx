6d6b5d091d630b8e50565399b53f1154c1a7781a
Carlos Frias <carlos.a.frias@gmail.com>
2020-11-12 10:54:57 -0300

    Update schedule

--- src/pages/Registration/index.tsx
+++ src/pages/Registration/index.tsx
@@ -14,8 +14,10 @@ const Registration: React.FunctionComponent = () => (
       <h2>¿Cómo inscribirse?</h2>
       <p className="text-container">
         Llene el <a href="#subs_form">formulario de inscripción</a> y nos
-        pondremos en contacto con usted a la brevedad. Cualquier consulta puede
-        ser dirigida a:{" "}
+        pondremos en contacto con usted a la brevedad.
+      </p>
+      <p className="text-container">
+        Cualquier consulta puede ser dirigida a:{" "}
         <a href="mailto:ponencias.simposio@gmail.com">
           ponencias.simposio@gmail.com
         </a>
